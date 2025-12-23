baec7bac588eccc6ec35cb6e21e0f59b8e8255c4
Amiri Ikram <ikram98712am@gmail.com>
2025-12-23 19:34:02 +0100

    update code

--- app_flask.py
+++ app_flask.py
@@ -1588,5 +1588,5 @@ if __name__ == "__main__":
     print("✓ Pas d'abréviations k/M dans les nombres")
     print("✓ Appuyez sur Ctrl+C pour arrêter\n")
     print("-" * 70)
-    
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
